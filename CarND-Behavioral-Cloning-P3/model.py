b7284a8e9c9fc36ebb44c913f0a0ae7cde263035
Samunda Perera <samunda@example.com>
2018-05-28 22:01:14 +0930

    Updated model

--- CarND-Behavioral-Cloning-P3/model.py
+++ CarND-Behavioral-Cloning-P3/model.py
@@ -33,6 +33,7 @@ def get_list_of_data(driving_log_file, use_flipped_image, use_left_right_images,
             # format: [file name, flip left-right?, angle]
             samples.append([center_image_name, False, center_angle])
 
+            # if flipping image left-right, negate the angle
             if use_flipped_image:
                 samples.append([center_image_name, True, -center_angle])
 
@@ -40,12 +41,15 @@ def get_list_of_data(driving_log_file, use_flipped_image, use_left_right_images,
                 left_image_name = line[1].split('/')[-1]
                 right_image_name = line[2].split('/')[-1]
 
+                # positive correction to (slightly) turn right
                 left_angle = center_angle + correction
+                # negative correction to (slightly) turn left
                 right_angle = center_angle - correction
 
                 samples.append([left_image_name, False, left_angle])
                 samples.append([right_image_name, False, right_angle])
 
+                # if flipping image left-right, negate the angle
                 if use_flipped_image:
                     samples.append([left_image_name, True, -left_angle])
                     samples.append([right_image_name, True, -right_angle])
@@ -78,12 +82,17 @@ def train(driving_log_file, use_flipped_image, use_left_right_images, correction
                 angles = []
 
                 for batch_sample in batch_samples:
+                    # load image
                     image_name = image_data_dir + batch_sample[0]
                     image = cv2.imread(image_name)
+
+                    # convert from BGR color mode (used by opencv) to RGB mode
+                    image = image[:, :, ::-1]
 
                     flip = batch_sample[1]
                     angle = batch_sample[2]
 
+                    # if value of 'flip' is true then, flip image left-right
                     if flip:
                         images.append(np.fliplr(image))
                     else:
@@ -104,31 +113,38 @@ def train(driving_log_file, use_flipped_image, use_left_right_images, correction
 
     model = Sequential()
 
-    # pre-process data
-    model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
-
     # crop top 70 pixels and bottom 25 pixels
-    model.add(Cropping2D(cropping=((70, 25), (0, 0))))
+    model.add(Cropping2D(cropping=((70, 25), (0, 0)), input_shape=(160, 320, 3)))
 
+    # pre-process data (normalise to range [-0.5, 0.5])
+    model.add(Lambda(lambda x: x / 255.0 - 0.5))
+
+    # convolutional layers for feature extraction
     model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation='relu'))
     model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation='relu'))
     model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation='relu'))
+    model.add(Dropout(drop_prob / 5.0))
 
     model.add(Convolution2D(64, 3, 3, activation='relu'))
-
     model.add(Convolution2D(64, 3, 3, activation='relu'))
-    model.add(Dropout(drop_prob))
+    model.add(Dropout(drop_prob / 5.0))
 
     model.add(Flatten())
 
-    model.add(Dense(100))
+    # fully-connected layers as a steering controller
+    model.add(Dense(100, activation='relu'))
     model.add(Dropout(drop_prob))
 
-    model.add(Dense(50))
-    model.add(Dense(10))
-    model.add(Dense(1))
+    model.add(Dense(50, activation='relu'))
+    model.add(Dropout(drop_prob))
 
+    model.add(Dense(10, activation='relu'))
+    model.add(Dense(1, activation=None))
+
+    # minimise MSE
     model.compile(loss='mse', optimizer='adam')
+
+    model.summary()
 
     samples_per_epoch = len(train_samples)
     nb_val_samples = len(validation_samples)
@@ -163,12 +179,12 @@ if __name__ == '__main__':
     use_flipped_image = True
 
     list_of_drop_probs = [0.5]
-    list_of_epochs = [4, 8]
-    list_of_corrections = [0.18, 0.20]
+    list_of_epochs = [5, 7]
+    list_of_corrections = [0.20]
 
-    batch_size = 64
+    batch_size = 256
 
-    # train models for each combination of parameters
+    # train model for each combination of parameters
     for drop_prob in list_of_drop_probs:
 
         for num_epochs in list_of_epochs:
